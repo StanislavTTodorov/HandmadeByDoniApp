e6cd9165339f4a7073b702efe64cf904576cb98d
Stanislav Todorov <77863355+StanislavTTodorov@users.noreply.github.com>
2024-03-14 09:30:54 +0200

    SeedCategory

--- HandmadeByDoniApp.Data/Configuration/GlassCategoryConfiguration.cs
+++ HandmadeByDoniApp.Data/Configuration/GlassCategoryConfiguration.cs
@@ -10,7 +10,57 @@ namespace HandmadeByDoniApp.Data.Configuration
     {
         public void Configure(EntityTypeBuilder<GlassCategory> builder)
         {
-           
+
+            builder.HasData(SeedCategories());
+        }
+        private ICollection<GlassCategory> SeedCategories()
+        {
+
+            GlassCategory wineCategory = new GlassCategory()
+            {
+                Id = 1,
+                Name = "Wine glasse"
+            };
+
+            GlassCategory beerCategory = new GlassCategory()
+            {
+                Id = 2,
+                Name = "Beer glass"
+            };
+
+            GlassCategory cognacCategory = new GlassCategory()
+            {
+                Id = 3,
+                Name = "Cognac glass"
+            };
+            GlassCategory whiskeyCategory = new GlassCategory()
+            {
+                Id = 4,
+                Name = "Whiskey glass"
+            };
+
+            GlassCategory teaCategory = new GlassCategory()
+            {
+                Id = 5,
+                Name = "Tea cup"
+            };
+
+            GlassCategory champagneCategory = new GlassCategory()
+            {
+                Id = 6,
+                Name = "champagne glass"
+            };
+
+            ICollection<GlassCategory> glassCategory = new HashSet<GlassCategory>();
+
+            glassCategory.Add(wineCategory);
+            glassCategory.Add(beerCategory);
+            glassCategory.Add(cognacCategory);
+            glassCategory.Add(whiskeyCategory);
+            glassCategory.Add(teaCategory);
+            glassCategory.Add(champagneCategory);
+
+            return glassCategory;
         }
     }
 }
---
+++ HandmadeByDoniApp.Data/Migrations/20240314072956_SeedCategory.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HandmadeByDoniApp.Data.Migrations
+{
+    public partial class SeedCategory : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "GlassCategories",
+                columns: new[] { "Id", "Name" },
+                values: new object[,]
+                {
+                    { 1, "Wine glasse" },
+                    { 2, "Beer glass" },
+                    { 3, "Cognac glass" },
+                    { 4, "Whiskey glass" },
+                    { 5, "Tea cup" },
+                    { 6, "champagne glass" }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "GlassCategories",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "GlassCategories",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "GlassCategories",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "GlassCategories",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "GlassCategories",
+                keyColumn: "Id",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "GlassCategories",
+                keyColumn: "Id",
+                keyValue: 6);
+        }
+    }
+}
