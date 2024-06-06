fede7d6ae33ce3ad5d99fd0bf9a4b0542f166404
Stanislav Todorov <77863355+StanislavTTodorov@users.noreply.github.com>
2024-06-06 09:58:52 +0300

    Implement ProductFormModel

---
+++ HandmadeByDoniApp.Web.ViewModels/Product/ProductFormModel.cs
@@ -0,0 +1,33 @@
+﻿using HandmadeByDoniApp.Web.ViewModels.Category;
+using System.ComponentModel.DataAnnotations;
+using static HandmadeByDoniApp.Common.EntityValidationConstants.Product;
+
+namespace HandmadeByDoniApp.Web.ViewModels.Product
+{
+    public class ProductFormModel
+    {
+        public ProductFormModel()
+        {
+            this.Categories = new HashSet<SelectCategoryFormModel>();
+        }
+
+        [Required]
+        [StringLength(TitleMaxLength, MinimumLength = TitleMinLength)]
+        public string Title { get; set; } = null!;
+
+        [StringLength(DescriptionMaxLength)]
+        public string? Description { get; set; }
+
+        [Required]
+        [Display(Name = "Image Link")]
+        public string ImageUrl { get; set; } = null!;
+
+        [Range(typeof(decimal), PriceMinLength, PriceMaxLength)]
+        public decimal Price { get; set; }
+
+        [Display(Name = "Category")]
+        public int CategoryId { get; set; }
+
+        public IEnumerable<SelectCategoryFormModel> Categories { get; set; }
+    }
+}
