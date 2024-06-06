f029e5960afc5b822cb310ad11e62ab995370dd5
Stanislav Todorov <77863355+StanislavTTodorov@users.noreply.github.com>
2024-06-06 08:57:03 +0300

    Implement Category Entity

---
+++ HandmadeByDoniApp.Data.Models/Category.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using static HandmadeByDoniApp.Common.EntityValidationConstants.Categoty;
+
+namespace HandmadeByDoniApp.Data.Models
+{
+    public class Category
+    {
+        public Category()
+        {
+            this.Products = new HashSet<Product>();
+        }
+
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(NameMaxLength)]
+        public string Name { get; set; } = null!;
+
+        public virtual ICollection<Product> Products { get; set; }
+    }
+}
