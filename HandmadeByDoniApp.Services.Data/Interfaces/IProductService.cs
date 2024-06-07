b01923671c1b3fe85fa79288c1a83c8274bf901b
Stanislav Todorov <77863355+StanislavTTodorov@users.noreply.github.com>
2024-06-07 10:00:00 +0300

    Add Product Service

--- HandmadeByDoniApp.Services.Data/Interfaces/IProductService.cs
+++ HandmadeByDoniApp.Services.Data/Interfaces/IProductService.cs
@@ -1,5 +1,6 @@
 ﻿
 
+using HandmadeByDoniApp.Data.Models;
 using HandmadeByDoniApp.Servises.Data.Models.Product;
 using HandmadeByDoniApp.Web.ViewModels.Box;
 using HandmadeByDoniApp.Web.ViewModels.Comment;
@@ -19,7 +20,18 @@ namespace HandmadeByDoniApp.Services.Data.Interfaces
 
         Task CreateProductAsync(ProductFormModel formModel);
 
+       // Task<GlassDetailsViewModel> GetProductDetailsByIdAsync(string glassId);
 
+        // Task<AllProductCommentViewModel> GetProductCommentByIdAsync(string glassId);
 
+        Task CreateCommentByUserIdAndByProductIdAsync(string userId, CommentFormModel formModel, string productId);
+
+        Task<ProductFormModel> GetProductForEditByIdAsync(string id);
+
+        Task EditProductByIdAndFormModelAsync(string id, ProductFormModel formModel);
+
+        Task SoftDeleteByIdAsync(string id);
+
+        Task RecoveryByIdAsync(string id);
     }
 }
