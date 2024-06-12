2cb9b35856053aeda049cec519f850a1085ec6ff
Stanislav Todorov <77863355+StanislavTTodorov@users.noreply.github.com>
2024-06-12 14:35:59 +0300

    fix action in ProductController

--- HandmadeByDoniApp.Services.Data/Interfaces/IProductService.cs
+++ HandmadeByDoniApp.Services.Data/Interfaces/IProductService.cs
@@ -1,10 +1,8 @@
 ﻿
-
-using HandmadeByDoniApp.Data.Models;
 using HandmadeByDoniApp.Servises.Data.Models.Product;
-using HandmadeByDoniApp.Web.ViewModels.Box;
+
 using HandmadeByDoniApp.Web.ViewModels.Comment;
-using HandmadeByDoniApp.Web.ViewModels.Glass;
+
 using HandmadeByDoniApp.Web.ViewModels.Home;
 using HandmadeByDoniApp.Web.ViewModels.Product;
 
@@ -20,10 +18,6 @@ namespace HandmadeByDoniApp.Services.Data.Interfaces
 
         Task CreateProductAsync(ProductFormModel formModel);
 
-       // Task<GlassDetailsViewModel> GetProductDetailsByIdAsync(string glassId);
-
-        // Task<AllProductCommentViewModel> GetProductCommentByIdAsync(string glassId);
-
         Task CreateCommentByUserIdAndByProductIdAsync(string userId, CommentFormModel formModel, string productId);
 
         Task<ProductFormModel> GetProductForEditByIdAsync(string id);
@@ -33,7 +27,9 @@ namespace HandmadeByDoniApp.Services.Data.Interfaces
         Task SoftDeleteByIdAsync(string id);
 
         Task RecoveryByIdAsync(string id);
+
         Task<ProductViewModel> GetProductDetailsByIdAsync(string id);
+
         Task<ProductCommentViewModel> GetProductCommentByIdAsync(string id);
     }
 }
