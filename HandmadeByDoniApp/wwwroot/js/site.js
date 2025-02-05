3e51d0901e5eeb17ca5b72ba8c50d99755bd6396
Stanislav Todorov <sttarrow@gmail.com>
2025-02-05 10:22:38 +0200

    culture

--- HandmadeByDoniApp/wwwroot/js/site.js
+++ HandmadeByDoniApp/wwwroot/js/site.js
@@ -2,3 +2,24 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+$(document).ready(function () {
+    $('#culture-select').addClass('button-menu').select2({
+        minimumResultsForSearch: Infinity, // Скрива полето за търсене
+        templateResult: function (data) {
+            if (!data.element) return data.text;
+            var flagUrl = $(data.element).data('flag');
+            if (flagUrl) {
+                return $('<span><img src="' + flagUrl + '" style="width: 40px; margin-right: 5px;"> </span>');
+            }
+
+        },
+        templateSelection: function (data) {
+            if (!data.element) return data.text;
+            var flagUrl = $(data.element).data('flag');
+            if (flagUrl) {
+                return $('<span><img src="' + flagUrl + '" style="width: 40px; margin-right: 5px;"> </span>');
+            }
+            return data.text;
+        }
+    });
+});
